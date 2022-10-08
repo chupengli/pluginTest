0ae587b5e1abb4cce7052a5650c9c9f1e150485f
GitHub Action <action@github.com>
2022-10-08 03:52:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/chupengli/plugintest/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/chupengli/plugintest/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.chupengli.plugintest.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.chupengli.plugintest.MyBundle
 
 class MyApplicationService {
 
